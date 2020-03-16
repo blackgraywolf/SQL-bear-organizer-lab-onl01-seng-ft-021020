a46b793bb110e880adab7c4e44a09c4265af3c51
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-16 16:28:45 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2, "M", "Black", "Rage", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (Null, 2, "M", "Black", "Rage", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (1,"Mr. Chocolate", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (2,"Rowdy", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (3,"Tabitha", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (4,"Sergeant Brown", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (5,"Melissa", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (6,"Grinch", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (7,"Wendy", 2, "M", "Black", "Rage", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (8,Null, 2, "M", "Black", "Rage", 1);
